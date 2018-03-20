733ed5b6cdbff4a7fd5781fa1aa750a08c3e586e
guilherme.santos <guiga021@hotmail.com>
2018-03-20 08:12:20 -0300

    Teste com nova rota

---
+++ src/components/NovaRota.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  WHOU
+  {{msg}}
+</div>
+</template>
+
+<script>
+export default{
+  name: 'NovaRota',
+  data() {
+    return {
+      msg: 'PEPERONI',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
